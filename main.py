9b64ea6898fb0fe8e389c1919b8ce4dcaddbbd00
demeil1 <demeil2005@gmail.com>
2023-12-07 15:02:24 -0600

    removing the updating.py functions

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from databases.users_table import createUsersTable
 from databases.stocks_table import createStockTable
-from databases.updating import createUpdatingTable, updateUpdatingTableStocks
 from bot_config import *
 from bot import *
 from databases.timing import marketHours
@@ -10,12 +9,10 @@ def main():
 
     createUsersTable()
     createStockTable()
-    createUpdatingTable()
 
     # while True:
         # if not marketHours():
             # pass
-        # updateUpdatingTableStocks
     CLIENT.run(TOKEN)
 
     databaseCleanup()
