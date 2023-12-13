5513889f2fcbcb90d5a0e23fc85632a901a04071
demeil1 <demeil2005@gmail.com>
2023-12-13 15:55:23 -0600

    tinkering with async to see if it is worth it

--- databases/yf_scraper.py
+++ databases/yf_scraper.py
@@ -1,3 +1,4 @@
+import time # remove
 import asyncio
 import yfinance as yf
 
@@ -21,3 +22,42 @@ async def getValue(tickers):
     # results = zip(tickers, prices)
     return prices
    
+# def nascrape(ticker):
+#
+#     try:
+#         # Create a Tickerdd object for the stock you want to retrieve data for
+#         ticker = yf.Ticker(ticker)
+#
+#         # Get the real-time stock price
+#         current_price = ticker.history(period="1d")["Close"].iloc[0]
+#
+#         return current_price
+#     except:
+#         return None
+#
+# def nagetValue(tickers):
+#
+#     prices = [nascrape(ticker) for ticker in tickers]
+#     return prices
+#
+# async def main():
+#     t_old = time.time()
+#     v = await getValue(['AAPL'])
+#     t_new = time.time()
+#     print(v, t_new - t_old)
+#
+#     t_old = time.time()
+#     v = await getValue(['AAPL', "MSFT", "GOOG", "BRK-B", "BRK-A", "AMZN" "sgkjs"])
+#     t_new = time.time()
+#     print(v, t_new - t_old)
+#
+#     t_old = time.time()
+#     v = nagetValue(['AAPL'])
+#     t_new = time.time()
+#     print(v, t_new - t_old)
+#
+#     t_old = time.time()
+#     v = await getValue(['AAPL', "MSFT", "GOOG", "BRK-B", "BRK-A", "AMZN" "sgkjs"])
+#     t_new = time.time()
+#     print(v, t_new - t_old)
+
